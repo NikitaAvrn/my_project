6d11c5c9bbf0d6af7e3f22c5f4d0fea46da950e7
NikitaAvrn <mel16bit@gmail.com>
2024-11-29 19:41:10 +1200

    tshoot  hello, add b.py

---
+++ b.py
@@ -0,0 +1,4 @@
+import math
+
+def sum_numbers(a, b):
+    return a ** 2 + b ** 2 + math.sqrt(a * b)
--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
+import b
+
 print("Hello, Git!")
-print("Индексация это важно!")
+print("Индексация это важно!\n")
 
 ind = 0
 sum = 0
@@ -8,3 +10,10 @@ while ind <= 9:
     sum += ind
     print(sum, end = ' ')
     ind += 1
+
+print("\n")
+
+a = int(input())
+c = int(input())
+
+print(b.sum_numbers(a, c))
